11454bd344cccb3aa1e2016126361c9b98eccf86
rowinf <>
2024-10-31 22:59:21 -0700

    add player and circleshape

--- main.py
+++ main.py
@@ -3,18 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 BLACK = (0,0,0)
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
     while True:
         screen.fill(BLACK)
+        dt = clock.tick(60) / 1000
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
 
 if __name__ == "__main__":
     main()
